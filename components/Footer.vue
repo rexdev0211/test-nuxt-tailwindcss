0cd89435239a7ff54a173adef8edbb2c13165d86
rexdev0211 <cutebaby.rex123@protonmail.com>
2022-02-18 19:46:48 +0300

    small updates

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="h-16 rounded-t-xl border-2 border-trueGray-500 shadow-md shadow-trueGray-500 flex items-center justify-center text-teal-600 px-5">
-        <button class="bg-gradient-to-r from-teal-600 to-blue-500 hover:bg-blue-700 text-white py-2 px-16 rounded-xl">
-            Kostenlos Registrieren
-        </button>
-    </div>
+  <div class="h-16 rounded-t-xl border-2 border-trueGray-500 shadow-md shadow-trueGray-500 flex items-center justify-center text-teal-600 px-5">
+    <button class="bg-gradient-to-r from-teal-600 to-blue-500 hover:bg-blue-700 text-white py-2 px-16 rounded-xl">
+      Kostenlos Registrieren
+    </button>
+  </div>
 </template>
 
 <script>
